6c4ad8037e1e0afa033917206c41d2d915095cdd
mohammed khaja <mohammedkhaja@Admins-MacBook-Air.local>
2020-05-28 18:17:48 -0500

    First8

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ mongoose.connect(process.env.CONNECT_URL,{useUnifiedTopology: true, useNewUrlPar
 
 app.use(express.static(publicPath));
 
-//app.use('/user', user);
-
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.use(express.json());
+
+app.get('/user', user);
 
 //app.use('/products', require('./routes/api/productRoutes'));
 //app.use('/orders', require('./routes/api/orderRoutes'));
