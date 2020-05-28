313f4ac319d9b890d2e1956897e927e9759cbf70
mohammed khaja <mohammedkhaja@Admins-MacBook-Air.local>
2020-05-28 18:21:37 -0500

    First8

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const express = require('express');
 const app = express();
-const user = require('./routes/api/userRoutes');
+const user = require('./routes/api/productRoutes');
 const publicPath = path.join(__dirname, 'client', 'build');
 const port = process.env.PORT || 3000;
 
@@ -11,14 +11,14 @@ mongoose.connect(process.env.CONNECT_URL,{useUnifiedTopology: true, useNewUrlPar
      .catch(err => console.log(err));
 
 app.use(express.static(publicPath));
+app.use(express.json());
+
+app.use('/user', user);
 
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.use(express.json());
-
-app.use('/user', user);
 
 //app.use('/products', require('./routes/api/productRoutes'));
 //app.use('/orders', require('./routes/api/orderRoutes'));
