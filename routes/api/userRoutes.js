f904e6d9447213eb7dbaba545b9d514ccade1de5
mohammed khaja <mohammedkhaja@Admins-MacBook-Air.local>
2020-05-28 19:03:38 -0500

    First8

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const validator = require('validator');
+const auth = require('../../middleware/auth');
+const {Users} = require('../../Schemas/Users');
 
 router.get('/', async (req, res) => {
     
